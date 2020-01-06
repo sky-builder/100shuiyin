5fa8d816330485f38378ac1cc9c2f39920842881
liushaoquan <breakthroughTGW@gmail.com>
2020-01-06 12:02:47 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ls -l
 
-ssh root@47.240.77.251 -p "cd 100shuiyin && git checkout master && git pull && npm run build && ls -l ./build"
+ssh root@47.240.77.251 -t "cd 100shuiyin && git checkout master && git pull && npm run build && ls -l ./build"
 
 # sftp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 << EOF
 # put ./build/index.html /root/webhook/index.html
