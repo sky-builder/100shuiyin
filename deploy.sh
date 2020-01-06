cd5a36555072dc3ab72630e793d2a8b501570f34
liushaoquan <breakthroughTGW@gmail.com>
2020-01-06 12:13:02 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 ls -l
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 -t "cd 100shuiyin && git checkout ci_setup && git pull && npm run build && ls -l ./build"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 -t "cd 100shuiyin && git checkout ci_setup && git pull && npm run build && cp -r build/* /var/www/html"
 
 # sftp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 << EOF
 # put ./build/index.html /root/webhook/index.html
