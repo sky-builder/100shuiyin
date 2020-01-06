6bf15ed4c259df7e0a2939e99c3b87304ec939bb
liushaoquan <breakthroughTGW@gmail.com>
2020-01-06 16:17:29 +0800

    feat: add travis-ci

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 -t "cd 100shuiyin && git checkout master && git pull && npm run build && cp -r build/* /var/www/html"
+
+# sftp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 << EOF
+# put -r ./build/* /var/www/html
+# EOF
