f3abca8d7c79428bae4826613d846d499acdcc7d
liushaoquan <breakthroughTGW@gmail.com>
2020-01-06 12:15:05 +0800

    dev

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello,world
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hello,travis-ci
+        </a>
+      </header>
     </div>
   );
 }
