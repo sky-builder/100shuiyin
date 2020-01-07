e8c25f43fd7887ab80f394f20f95d9192d4c8f3d
liushaoquan <breakthroughTGW@gmail.com>
2020-01-07 08:01:55 +0800

    dev

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const AppLogo = React.memo(props => {
 });
 
 const AppAction = props => {
+  const { activeLogo, setActiveLogo } = props;
   function exportImage() {
     let canvas = document.querySelector('.app__bg');
     let a = document.createElement('a');
@@ -49,13 +50,54 @@ const AppAction = props => {
     props.setPageStage(1);
     props.setLogoList([]);
   }
+  useEffect(() => {
+    console.log('update on activeLogo');
+    if (activeLogo && activeLogo.type === 'text') {
+      let el = activeLogo.el;
+    }
+  }, [activeLogo, setActiveLogo]);
+  el.addEventListener('mousemove', e => {
+    console.log('el mousemove', activeLogo);
+    if (!activeLogo) return;
+      let x = e.movementX;
+      let y = e.movementY;
+      activeLogo.x += x;
+      activeLogo.y += y;
+      activeLogo.el.style.left = activeLogo.x + 'px';
+      activeLogo.el.style.top = activeLogo.y + 'px';
+  });
+  el.addEventListener('mouseup', e => {
+    console.log('el mouseup', activeLogo);
+    setActiveLogo(null);
+    console.log('it works')
+  });
+  function addText() {
+    let el = document.createElement('div');
+    el.textContent = 'hello,world';
+    el.style.position = 'absolute';
+    el.style.userSelect = 'none';
+    document.body.appendChild(el);
+    let canvas = document.querySelector('.app__bg');
+    let rect = canvas.getBoundingClientRect();
+    let textObj = {
+      x: rect.x,
+      y: rect.y,
+      w: 0,
+      h: 0,
+      type: 'text',
+      el: el
+    };
+    el.addEventListener('mousedown', e => {
+      console.log('mousedown');
+      setActiveLogo(textObj);
+    });
+    props.setLogoList(props.logoList.concat([textObj]));
+    console.log('add text');
+  }
   if (props.pageStage === EDIT_STAGE) {
     return (
       <div className="app__action">
-        <button
-          className="app__delete button is-danger"
-          onClick={changeStage}
-        >
+        <button className="app__delete button is-danger" onClick={changeStage}>
           delete
         </button>
         <button className="app__upload button is-info ">
@@ -70,6 +112,9 @@ const AppAction = props => {
         </button>
         <button className="app__export button is-success" onClick={exportImage}>
           export
+        </button>
+        <button className="app__add-text button is-info" onClick={addText}>
+          add text
         </button>
       </div>
     );
@@ -94,9 +139,15 @@ const AppBody = props => {
   }
 };
 const AppMain = props => {
-  const [activeLogo, setActiveLogo] = useState(null);
-  const {bgImage, setBgImage, pageStage, logoList} = props;
-  const {img, name, scale} = bgImage
+  const {
+    bgImage,
+    setBgImage,
+    pageStage,
+    logoList,
+    activeLogo,
+    setActiveLogo
+  } = props;
+  const { img, name, scale } = bgImage;
 
   useEffect(() => {
     function handleWindowResize() {
@@ -114,7 +165,7 @@ const AppMain = props => {
         img: img,
         name: name,
         scale: newScale
-      })
+      });
       canvas.style.transform = 'scale(' + newScale + ')';
       appBody.style.height = appBody.offsetHeight + 'px';
       canvas.width = nw;
@@ -122,13 +173,19 @@ const AppMain = props => {
       let ctx = canvas.getContext('2d');
       ctx.drawImage(img, 0, 0);
       for (let i = 0; i < logoList.length; i += 1) {
-        ctx.drawImage(logoList[i].img, logoList[i].x, logoList[i].y);
+        if (logoList[i].type === 'text') {
+          logoList[i].el.style.left = logoList[i].x + 'px';
+          logoList[i].el.style.top = logoList[i].y + 'px';
+        } else {
+          ctx.drawImage(logoList[i].img, logoList[i].x, logoList[i].y);
+        }
       }
     }
+    console.log('update on canvas');
     handleWindowResize();
     window.addEventListener('resize', handleWindowResize);
     return () => window.removeEventListener('resize', handleWindowResize);
-  }, [img, logoList, name, pageStage, setBgImage]);
+  }, [activeLogo, img, logoList, name, pageStage, setBgImage]);
 
   function handleMouseDown(e) {
     let canvas = document.querySelector('.app__bg');
@@ -140,19 +197,25 @@ const AppMain = props => {
       scale;
     for (let i = logoList.length - 1; i >= 0; i -= 1) {
       let logo = logoList[i];
-      if (
-        x >= logo.x &&
-        x <= logo.w + logo.x &&
-        y >= logo.y &&
-        y <= logo.h + logo.y
-      ) {
-        setActiveLogo(logo);
-        return;
+      if (logo.type === 'text') {
+        continue;
+      } else if (logo.type === 'image') {
+        if (
+          x >= logo.x &&
+          x <= logo.w + logo.x &&
+          y >= logo.y &&
+          y <= logo.h + logo.y
+        ) {
+          console.log('1')
+          setActiveLogo(logo);
+          return;
+        }
       }
     }
   }
 
   function handleMouseMove(e) {
+    console.log('canvas mousemove', !!activeLogo);
     if (!activeLogo) return;
     let canvas = document.querySelector('.app__bg');
     let x = activeLogo.x + e.movementX / scale;
@@ -163,11 +226,17 @@ const AppMain = props => {
     activeLogo.y = y;
     ctx.drawImage(img, 0, 0);
     for (let i = 0; i < logoList.length; i += 1) {
-      ctx.drawImage(logoList[i].img, logoList[i].x, logoList[i].y);
+      if (logoList[i].type === 'text') {
+        logoList[i].el.style.left = logoList[i].x + 'px';
+        logoList[i].el.style.top = logoList[i].y + 'px';
+      } else {
+        ctx.drawImage(logoList[i].img, logoList[i].x, logoList[i].y);
+      }
     }
   }
 
   function handleMouseUp(e) {
+    console.log('2');
     setActiveLogo(null);
   }
 
@@ -210,6 +279,26 @@ const AppWelcome = props => {
     });
     reader.readAsDataURL(e.target.files[0]);
   }
+  function dev() {
+    let img = new Image();
+    img.src = 'https://picsum.photos/600';
+    img.addEventListener('load', () => {
+      let appBody = document.querySelector('.app__body');
+      let bw = appBody.offsetWidth;
+      let bh = appBody.offsetHeight;
+      let s1 = bw / img.naturalWidth;
+      let s2 = bh / img.naturalHeight;
+      let scale = Math.min(s1, s2);
+      if (scale > 1) scale = 1;
+      props.setBgImage({
+        img,
+        name: 'mark',
+        scale
+      });
+      props.setPageStage(EDIT_STAGE);
+    });
+  }
+  dev();
   return (
     <div className="app__welcome">
       <label htmlFor="upload-bg-image">选择背景图片</label>
@@ -223,6 +312,7 @@ function App() {
   const [pageStage, setPageStage] = useState(WELCOME_STAGE);
   const [logoList, setLogoList] = useState([]);
   const [bgImage, setBgImage] = useState();
+  const [activeLogo, setActiveLogo] = useState(null);
 
   let chidlProps = {
     pageStage,
@@ -230,7 +320,9 @@ function App() {
     bgImage,
     setBgImage,
     logoList,
-    setLogoList
+    setLogoList,
+    activeLogo,
+    setActiveLogo
   };
   return (
     <div className="app">
