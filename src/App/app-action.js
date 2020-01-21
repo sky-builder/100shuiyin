9b359e26f9f93e3284be9abd5ee02b53fbb724c4
liushaoquan <breakthroughTGW@gmail.com>
2020-01-21 14:22:39 +0800

    fix: export image has outline

--- src/App/app-action.js
+++ src/App/app-action.js
@@ -3,6 +3,7 @@ import { PAGE_STAGE, ACTION, OBJECT_TYPE } from '../js/enum';
 import { exportCanvas } from '../js/utility';
 
 const AppAction = props => {
+  const { bgImage, logoList } = props;
   function toWelcomeStage() {
     props.setPageStage(PAGE_STAGE.WELCOME);
     props.setLogoList([]);
@@ -37,7 +38,35 @@ const AppAction = props => {
     props.setLogoList(props.logoList.concat([obj]));
   }
   function exportImage() {
+    const { img } = bgImage;
     let canvas = document.querySelector('.app__bg');
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0);
+    for (let i = 0; i < logoList.length; i += 1) {
+      ctx.save();
+      ctx.globalAlpha = logoList[i].opacity || 1;
+      let cx = logoList[i].x + logoList[i].w / 2;
+      let cy = logoList[i].y + logoList[i].h / 2;
+      ctx.translate(cx, cy);
+      ctx.rotate((logoList[i].angle * Math.PI) / 180);
+      ctx.translate(-cx, -cy);
+      if (logoList[i].objectType === OBJECT_TYPE.IMAGE) {
+        ctx.drawImage(
+          logoList[i].img,
+          logoList[i].x,
+          logoList[i].y,
+          logoList[i].w,
+          logoList[i].h
+        );
+      } else if (logoList[i].objectType === OBJECT_TYPE.TEXT) {
+        let { text, x, y, h, w } = logoList[i];
+        ctx.textBaseline = 'top';
+        ctx.font = `${h}px serif`;
+        ctx.fillText(text, x, y, w);
+      }
+      ctx.restore();
+    }
     exportCanvas(canvas, props.bgImage.name.split('.')[0]);
   }
   function getTextObject() {
