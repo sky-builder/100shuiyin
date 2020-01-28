62f58301840f06c2088867c7a16d55e64e1ff18e
lsq <liushaoquan@aiyunxiao.com>
2020-01-28 12:40:37 +0800

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 -t "cd 100shuiyin && git checkout master && git pull && npm run build && cp -r build/* /var/www/html"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 -t "cd 100shuiyin && git checkout master && git pull -f && npm run build && cp -r build/* /var/www/html"
 
 # sftp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 << EOF
 # put -r ./build/* /var/www/html
