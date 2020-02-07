bfde678e929d1ca5ea30ea074a51ed5602e0c93c
lsq <breakthroughtgw@gmail.com>
2020-02-07 20:18:51 +0800

    ci: run npm install before build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 -t "cd 100shuiyin && git checkout master && git fetch origin master && git reset --hard FETCH_HEAD && npm run build && cp -r build/* /var/www/html"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 -t "cd 100shuiyin && git checkout master && git fetch origin master && git reset --hard FETCH_HEAD && npm install & npm run build && cp -r build/* /var/www/html"
 
 # sftp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@47.240.77.251 << EOF
 # put -r ./build/* /var/www/html
